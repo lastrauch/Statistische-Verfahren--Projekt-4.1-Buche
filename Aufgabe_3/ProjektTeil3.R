0d952b97c0fc89bbeaa1c7d54d79cef322f3e06c
KamiMatze <matthias.krassmann@hotmail.de>
2020-03-16 00:11:22 +1100

    ProjektTeil3

--- Aufgabe_3/ProjektTeil3.R
+++ Aufgabe_3/ProjektTeil3.R
@@ -37,60 +37,37 @@ numberObs = nrow(buche.data.used)
 cross.length.out = numberObs
 
 # Initialisierung
-SPSE1 =SPSE2 =SPSE3 =SPSE4 =SPSE5 =SPSE6 =SPSE7=SPSE8 =0
+SPSE = c(1:4)
 
 ## Kreuzalidierung
-index = rep(1:5, length.out = cross.length.out) # 80 % Trainingsdaten
+index = rep(1:10, length.out = cross.length.out)
 
-
-for(j in 1:100){
-  index = sample(index)
-  i = 1
-  ## Zerlegung
-  buche.test= buche.data.used[index==i,]
-  buche.train= buche.data.used[index!=i,]
-
-  ## Parameterschaetzung
-  m1 <- lm(log(biom)~1+ I(log(dbh)), data =buche.train)
-  m2 <- lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2), data =buche.train)
-  m3 <- lm(log(biom)~1+ I(log(dbh))+I(log(height)), data =buche.train)
-  m4 <- lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(height))+I(log(height)^2)+I(log(age))+hsl, data =buche.train)
-  # m5 <- lm(log(biom)~(1+ I(log(dbh))+I(log(height))+I(log(age))+hsl)*as.factor(author), data =buche.train)
- 
-  ## Prognosefehler
-  SPSE1 = SPSE1 + sum((log(buche.test$biom)-predict(m1, newdata=buche.test))^2)
-  SPSE2 = SPSE2 + sum((log(buche.test$biom)-predict(m2, newdata=buche.test))^2)
-  SPSE3 = SPSE3 + sum((log(buche.test$biom)-predict(m3, newdata=buche.test))^2)
-  SPSE4 = SPSE4 + sum((log(buche.test$biom)-predict(m4, newdata=buche.test))^2)
-  # SPSE5 = SPSE5 + sum((log(buche.test$biom)-predict(m5, newdata=buche.test))^2)
-
+for(m in 2:5){
+  for(j in 1:100){
+    index = sample(index)
+    i = c(1:m)
+    ## Zerlegung
+    buche.test= buche.data.used[index %in% i,]
+    buche.train= buche.data.used[!(index %in% i),]
+  
+    ## Parameterschaetzung
+    m.joosten = lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(age))+I(log(height))+I(log(height)^2)+hsl+I(log(hsl))+I(log(hsl)^2), data = buche.train)
+   
+    ## Prognosefehler
+    SPSE[m-1] = SPSE[m-1] + sum((log(buche.test$biom)-predict(m.joosten, newdata=buche.test))^2)
+  }
 }
-c(SPSE1 ,SPSE2 ,SPSE3 ,SPSE4)
-
 
 ## Schätzung auf Grund von RSS
-m1 <- lm(log(biom)~1+ I(log(dbh)), data =buche.train)
-m2 <- lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2), data =buche.train)
-m3 <- lm(log(biom)~1+ I(log(dbh))+I(log(height)), data =buche.train)
-m4 <- lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(height))+I(log(height)^2)+I(log(age))+hsl, data =buche.train)
-# m5 <- lm(log(biom)~(1+ I(log(dbh))+I(log(height))+I(log(age))+hsl)*as.factor(author), data =buche.train)
+m.joosten = lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(age))+I(log(height))+I(log(height)^2)+hsl+I(log(hsl))+I(log(hsl)^2), data = buche.test)
 
-RSS1 = sum(residuals(m1)^2)
-RSS2 = sum(residuals(m2)^2)
-RSS3 = sum(residuals(m3)^2)
-RSS4 = sum(residuals(m4)^2)
-# RSS5 = sum(residuals(m5)^2)
+RSS = sum(residuals(m.joosten)^2)
 
 
-c(RSS1, RSS2, RSS3, RSS4)
-summary(m4)
-sigma2.max = RSS4/(cross.length.out-5) # Wie wird sigma2.max berechnet? woher kam bei aufgabe 8 die -19? Number of coefficients in max model?
+summary(m.joosten)
+sigma2.max = RSS/(cross.length.out-9) # Wie wird sigma2.max berechnet? woher kam bei aufgabe 8 die -19? Number of coefficients in max model?
 
-SPSE1 = RSS1 + 2*sigma2.max*length(coef(m1))
-SPSE2 = RSS2 + 2*sigma2.max*length(coef(m2))
-SPSE3 = RSS3 + 2*sigma2.max*length(coef(m3))
-SPSE4 = RSS4 + 2*sigma2.max*length(coef(m4))
-# SPSE5 = RSS5 + 2*sigma2.max*length(coef(m5))
+SPSE1 = RSS + 2*sigma2.max*length(coef(m.joosten))
 
 c(SPSE1 ,SPSE2 ,SPSE3 ,SPSE4)
 
@@ -110,3 +87,4 @@ summary(buche.bss)$cp
 summary(buche.bss)$which[7,]
 
 summary(buche.bss)$cp*sigma2.max + cross.length.out* sigma2.max
+
