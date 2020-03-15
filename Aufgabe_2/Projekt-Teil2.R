1cc1d7268b7e605445831c48c6c4ebd99f484df8
Julius Isken <julius.isken@uni-jena.de>
2020-03-15 12:10:12 +0100

    A1

--- Aufgabe_2/Projekt-Teil2.R
+++ Aufgabe_2/Projekt-Teil2.R
@@ -1,5 +1,5 @@
 # Setzen des Working Directory
-setwd("C:/Users/Matthias/OneDrive/SV/Projekt")
+# setwd("~/Documents/Master Data Science/Statistische Verfahren/Projekt/Statistische-Verfahren--Projekt-4.1-Buche/Aufgabenstellung")
 # Laden der Daten aus CSV
 buche.data = read.csv("buche.csv", sep=",")
 head(buche.data)
@@ -12,14 +12,15 @@ head(buche.data)
 # leaps
 require("leaps")
 
-buche.bss = regsubsets(log(biom)~(1+ I(log(dbh))+I(log(dbh)^2)+I(log(age))+I(log(age)^2)+I(log(height))+I(log(height)^2)+hsl+I(hsl^2)+I(log(hsl))+I(log(hsl)^2))*as.factor(author), data = buche.data, nbest = 3) ## nbest = 3, jeweils die drei besten Modelle gleicher Parameterzahl
+buche.bss = regsubsets(log(biom)~(1+ I(log(dbh))+I(log(dbh)^2)+I(log(age))+I(log(age)^2)+I(log(height))+I(log(height)^2)+hsl+I(hsl^2)+I(log(hsl))+I(log(hsl)^2))*as.factor(author), data = buche.data, nbest = 3)
+
+## nbest = 3, jeweils die drei besten Modelle gleicher Parameterzahl
 # summary(buche.bss)
 summary(buche.bss)$cp
 summary(buche.bss)$which[22,] # Modell 22 liefert betragsmaessig besten CP-Wert (4.248051)
 
 # Bestes Modell mit Autor als kategorielle Einflussgroesse
-# m = lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(height))+I(log(height)^2)+as.factor(author)Joosten+I(log(height)):as.factor(author)Joosten+I(log(height)^2):as.factor(author)Joosten+I(hsl^2):as.factor(author)Joosten, data = buche.data)
-m = lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(height))+I(log(height)^2)+as.factor(author)+I(log(height)):as.factor(author)+I(log(height)^2):as.factor(author)+I(hsl^2):as.factor(author), data = buche.data)
+m = lm(log(biom)~1+ I(log(dbh))+I(log(dbh)^2)+I(log(height))+I(log(height)^2)+as.factor(author == "Joosten")+I(log(height)):as.factor(author == "Joosten")+I(log(height)^2):as.factor(author == "Joosten")+I(hsl^2):as.factor(author == "Joosten"), data = buche.data)
 
 # Berechnen der Parameter fuer die Modelle
 summary(m)
